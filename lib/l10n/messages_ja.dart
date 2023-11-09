61f2b6b2d344a77ab9e19b5734d4e7e22e43417b
kindkmj <41276448+kindkmj@users.noreply.github.com>
2023-11-10 00:05:45 +0900

    초기 설정 완료

---
+++ lib/l10n/messages_ja.dart
@@ -0,0 +1,275 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a en locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final MessageLookup messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'ja';
+
+  final Map<String, Function> messages =
+      _notInlinedMessages(_notInlinedMessages);
+
+  static Map<String, Function> _notInlinedMessages(dynamic value) =>
+      <String, Function>{
+        'serviceDescription': MessageLookupByLibrary.simpleMessage(
+            'Promptを知らない誰でも簡単に高品質の画像を作成できます。'),
+        'noSearchImages': MessageLookupByLibrary.simpleMessage('イメージが見つかりません。'),
+        'noBookmarkedImages':
+            MessageLookupByLibrary.simpleMessage('お気に入りに登録したイメージがありません。'),
+        'gallery': MessageLookupByLibrary.simpleMessage('ギャラリー'),
+        'generate': MessageLookupByLibrary.simpleMessage('つくる'),
+        'aiEditor': MessageLookupByLibrary.simpleMessage('AI編集機能'),
+        'userSetting': MessageLookupByLibrary.simpleMessage('ユーザー設定'),
+        'askGPT': MessageLookupByLibrary.simpleMessage('ChatGPTへ聞いてみよう'),
+        'hasImageBeenCreated':
+            MessageLookupByLibrary.simpleMessage('まだ作成されたイメージがありません'),
+        'showCompanyname': MessageLookupByLibrary.simpleMessage('Company Name'),
+        'showCorporateName':
+            MessageLookupByLibrary.simpleMessage('DELABLE Inc.'),
+        'address': MessageLookupByLibrary.simpleMessage('Address'),
+        'showAddress': MessageLookupByLibrary.simpleMessage(
+            '5F Deungwoo Bldg., 533, Samsung-Ro, Gangnam-Gu, Seoul, Republic of Korea'),
+        'email': MessageLookupByLibrary.simpleMessage('E-mail'),
+        'phoneNumber': MessageLookupByLibrary.simpleMessage('Tel.'),
+        'showBusinessRegNumber':
+            MessageLookupByLibrary.simpleMessage('Business Registration No.'),
+        'showCommerceRegNumber':
+            MessageLookupByLibrary.simpleMessage('Commerce Registration No.'),
+        'showCEO': MessageLookupByLibrary.simpleMessage('CEO'),
+        'showCEOName': MessageLookupByLibrary.simpleMessage('Kyungho Kim'),
+        'showCISOName': MessageLookupByLibrary.simpleMessage('Lee, Jung-hun'),
+        'showPersonalInfoManager': MessageLookupByLibrary.simpleMessage(
+            'Personal Information Manager'),
+        'login': MessageLookupByLibrary.simpleMessage('スタート'),
+        'startForFree': MessageLookupByLibrary.simpleMessage('無料で始めよう'),
+        'showImagesFromOtherUsers':
+            MessageLookupByLibrary.simpleMessage('みんなが作成したイメージを見れます'),
+        'sortByGenerated': MessageLookupByLibrary.simpleMessage('最新作成順'),
+        'sortBySeen': MessageLookupByLibrary.simpleMessage('最新閲覧順'),
+        'whichImageGenerate':
+            MessageLookupByLibrary.simpleMessage('どんなイメージを作りますか？'),
+        'selectStyle': MessageLookupByLibrary.simpleMessage('スタイルㄴ選択'),
+        'profile': MessageLookupByLibrary.simpleMessage('プロフィール'),
+        'profileUpdate': MessageLookupByLibrary.simpleMessage('ユーザー設定'),
+        'settings': MessageLookupByLibrary.simpleMessage('設定'),
+        'inbox': MessageLookupByLibrary.simpleMessage('メッセージ'),
+        'subscription': MessageLookupByLibrary.simpleMessage('購読決済'),
+        'subscriptionDescription': MessageLookupByLibrary.simpleMessage(
+            '現在のベータ版サービスとして、制限なく画像を作成できます。\n6月からは前払い購読プランが、追加される予定です。'),
+        'notification': MessageLookupByLibrary.simpleMessage('プッシュ通知'),
+        'notificationSettings': MessageLookupByLibrary.simpleMessage('通知設定'),
+        'deleteAccount': MessageLookupByLibrary.simpleMessage('アカウント削除'),
+        'privacyPolicy': MessageLookupByLibrary.simpleMessage('個人情報保護処理方針'),
+        'termsOfUse': MessageLookupByLibrary.simpleMessage('サービス利用方針'),
+        'editProfilePhoto': MessageLookupByLibrary.simpleMessage('プロフィール写真更新'),
+        'name': MessageLookupByLibrary.simpleMessage('名前'),
+        'selfIntro': MessageLookupByLibrary.simpleMessage('自己紹介'),
+        'webPage': MessageLookupByLibrary.simpleMessage('ホームページ'),
+        'all': MessageLookupByLibrary.simpleMessage('全体'),
+        'unReads': MessageLookupByLibrary.simpleMessage('未読'),
+        'reads': MessageLookupByLibrary.simpleMessage('既読'),
+        'readAll': MessageLookupByLibrary.simpleMessage('全て既読へ'),
+        'eraseAll': MessageLookupByLibrary.simpleMessage('全て削除'),
+        'language': MessageLookupByLibrary.simpleMessage('言語'),
+        'korean': MessageLookupByLibrary.simpleMessage('한국어'),
+        'english': MessageLookupByLibrary.simpleMessage('English'),
+        'japanese': MessageLookupByLibrary.simpleMessage('日本語'),
+        'signOut': MessageLookupByLibrary.simpleMessage('ログアウト'),
+        'free': MessageLookupByLibrary.simpleMessage('無料'),
+        'justCheckWhatIcanDo':
+            MessageLookupByLibrary.simpleMessage('何が出来るか試してみます'),
+        'subscribe': MessageLookupByLibrary.simpleMessage('購読する'),
+        'lowQualityImageMax':
+            MessageLookupByLibrary.simpleMessage('低品質　デザイン　枚まで'),
+        'starter': MessageLookupByLibrary.simpleMessage('スターター'),
+        'canUseWhenNeeded': MessageLookupByLibrary.simpleMessage('必要なたびに使います'),
+        'highQualityImageMax':
+            MessageLookupByLibrary.simpleMessage('高品質　デザイン　枚まで'),
+        'fastestGenerationSpeed':
+            MessageLookupByLibrary.simpleMessage('最も早い作成スピード'),
+        'premium': MessageLookupByLibrary.simpleMessage('プレミアム'),
+        'canUseAsOftenAsPossible':
+            MessageLookupByLibrary.simpleMessage('頻繁に使います'),
+        'allNotifications': MessageLookupByLibrary.simpleMessage('全体プッシュ通知'),
+        'onOffToAllNotifications':
+            MessageLookupByLibrary.simpleMessage('全てのプッシュ通知について非活性、活性化します'),
+        'emailNotification': MessageLookupByLibrary.simpleMessage('メールでのお知らせ'),
+        'sendEmailForAnyNotifications':
+            MessageLookupByLibrary.simpleMessage('プッシュ通知内容についてメールでお知らせします'),
+        'detailNotification': MessageLookupByLibrary.simpleMessage('プッシュ通知の詳細'),
+        'notificationFromOtherUsers':
+            MessageLookupByLibrary.simpleMessage('他の利用者からの通知'),
+        'sendNotificationWhenOthersDownloadYourGeneratedImage':
+            MessageLookupByLibrary.simpleMessage(
+                '貴方が作ったイメージを他の利用者がダウンロードした場合、お知らせします'),
+        'notificationFromDesignStaff':
+            MessageLookupByLibrary.simpleMessage('デザインスタッフからの通知'),
+        'sendNotificationWhenImageIsGenerated':
+            MessageLookupByLibrary.simpleMessage('イメージが作成完了した時にお知らせします'),
+        'systemNotifications': MessageLookupByLibrary.simpleMessage('システム通知'),
+        'sendNotificationFromSystemNotifice':
+            MessageLookupByLibrary.simpleMessage(
+                'システム通知、利用プランの変更などシステムからお知らせします'),
+        'afterDeletingAccount': MessageLookupByLibrary.simpleMessage(
+            'アカウントを削除すると、全ての関連するデーターは削除されます'),
+        'whenYouWantToProceed': MessageLookupByLibrary.simpleMessage(
+            "アカウントを削除したいときは”delete account”を入力してください"),
+        'searchResults': MessageLookupByLibrary.simpleMessage('件の結果が検索されました'),
+        'designInformation': MessageLookupByLibrary.simpleMessage('デザイン情報'),
+        'designStaffTip': MessageLookupByLibrary.simpleMessage('役に立つコツ'),
+        'tip1': MessageLookupByLibrary.simpleMessage(
+            '✓ 人工知能が理解できるように簡潔な文章で書きましょう。'),
+        'tip2':
+            MessageLookupByLibrary.simpleMessage('✓ 動物は現在1つの画像に1種類だけを入力します。'),
+        'tip21': MessageLookupByLibrary.simpleMessage(
+            '例) 亀とうさぎが ~~(X), 小さな鳥が ~~(O)'),
+        'tip3': MessageLookupByLibrary.simpleMessage(
+            '✓ 動作や角度、背景など明確に提示してくれるのがいいです。'),
+        'tip31': MessageLookupByLibrary.simpleMessage('例) 横顔、全身、広い海、可愛い子犬など'),
+        'tip4': MessageLookupByLibrary.simpleMessage(
+            '✓ 主体に対する説明と（、）コンマ区切りで背景に対する説明で作る方が良いです。'),
+        'tip41':
+            MessageLookupByLibrary.simpleMessage('例) ２３年度秋ファッションの女性モデル、グレーの背景'),
+        'tip5': MessageLookupByLibrary.simpleMessage(
+            '✓ デザインスタッフはいつもあなたの要求を聞いています。'),
+        'tip51': MessageLookupByLibrary.simpleMessage(
+            '人工知能モデルとプロンプト自動生成処理は継続的に成長します。'),
+        'generatedUser': MessageLookupByLibrary.simpleMessage('作った利用者'),
+        'imageInformation': MessageLookupByLibrary.simpleMessage('イメージ情報'),
+        'promptInformation': MessageLookupByLibrary.simpleMessage('標準プロンプト情報'),
+        'download': MessageLookupByLibrary.simpleMessage('ダウンロード'),
+        'recommendSimilarImagesFromGallery':
+            MessageLookupByLibrary.simpleMessage('ギャラリーにある似たイメージ'),
+        'theMoreConciseAlongWithAdjectives':
+            MessageLookupByLibrary.simpleMessage(
+                '形容詞があり簡潔な文章であるほど望む結果を作る可能性が高まります'),
+        'showOnlyImadeByMe': MessageLookupByLibrary.simpleMessage('自分が作ったのみ探す'),
+        'realworldStyle': MessageLookupByLibrary.simpleMessage('リアルな写真'),
+        'paintingStyle': MessageLookupByLibrary.simpleMessage('油絵画像'),
+        'illustrationStyle': MessageLookupByLibrary.simpleMessage('イラスト'),
+        'withFigure': MessageLookupByLibrary.simpleMessage('人物含む'),
+        'sciFi': MessageLookupByLibrary.simpleMessage('SF'),
+        'abstract': MessageLookupByLibrary.simpleMessage('追想的'),
+        'detailLandScape': MessageLookupByLibrary.simpleMessage('鮮明な風景'),
+        'highQuality': MessageLookupByLibrary.simpleMessage('高品質'),
+        'vertical': MessageLookupByLibrary.simpleMessage('縦長'),
+        'horizontal': MessageLookupByLibrary.simpleMessage('横長'),
+        'save': MessageLookupByLibrary.simpleMessage('保存する'),
+        'month': MessageLookupByLibrary.simpleMessage('/月払い'),
+        'billedInformation': MessageLookupByLibrary.simpleMessage(
+            'デザインスタッフはサービス利用方針に基づき、定期購読型として毎月1日へ請求されます。購読した時点より満一ヶ月に至らない場合、日割り清算され請求されます'),
+        'subscriptionStartDate': MessageLookupByLibrary.simpleMessage('決済開始日'),
+        'subscriptionPlan': MessageLookupByLibrary.simpleMessage('利用プラーン'),
+        'lowQuality': MessageLookupByLibrary.simpleMessage('低品質'),
+        'inUse': MessageLookupByLibrary.simpleMessage('利用中'),
+        'mostUsed': MessageLookupByLibrary.simpleMessage('人気プラーン'),
+        'sketchStyle': MessageLookupByLibrary.simpleMessage('スケッチ'),
+        'message': MessageLookupByLibrary.simpleMessage('メッセージ'),
+        'profileImageEdit': MessageLookupByLibrary.simpleMessage('プロフィール写真変更'),
+        'profileImageEditMessage': MessageLookupByLibrary.simpleMessage(
+            '保存をクリックしないと、変更された画像は反映されません。'),
+        'otherPeople': MessageLookupByLibrary.simpleMessage('他のユーザーより'),
+        'nowMonth': MessageLookupByLibrary.simpleMessage('月現在'),
+        'generateCount': MessageLookupByLibrary.simpleMessage('枚作成'),
+        'voluntaryWithdrawal': MessageLookupByLibrary.simpleMessage('退会する'),
+        'requestMessage': MessageLookupByLibrary.simpleMessage('作成命令'),
+        'size': MessageLookupByLibrary.simpleMessage('サイズ'),
+        'pixel': MessageLookupByLibrary.simpleMessage('pixel'),
+        'otherDesign': MessageLookupByLibrary.simpleMessage('様の他の写真'),
+        'randomArray': MessageLookupByLibrary.simpleMessage('ランダム'),
+        'youMustSignInFirst':
+            MessageLookupByLibrary.simpleMessage('まずはログインをお願いします。'),
+        'noInputData': MessageLookupByLibrary.simpleMessage('입력 값이 없습니다.'),
+        'noSelectDesignStyle':
+            MessageLookupByLibrary.simpleMessage('이미지 모델을 선택해주세요.'),
+        'noSelectDesignShape':
+            MessageLookupByLibrary.simpleMessage('생성될 이미지 타입을 선택해주세요.'),
+        'planTitleFree': MessageLookupByLibrary.simpleMessage('無料'),
+        'planDescFree': MessageLookupByLibrary.simpleMessage('何が出来るか体験してみます。'),
+        'planPriceFree': MessageLookupByLibrary.simpleMessage('0'),
+        'planConditionFree1': MessageLookupByLibrary.simpleMessage(
+            '✓ ベーター公開して以降、低品質デザインを毎月20枚まで体験生成可能'),
+        'planCurrencyUnit': MessageLookupByLibrary.simpleMessage(r'$'),
+        'planTitleStart': MessageLookupByLibrary.simpleMessage('スターター'),
+        'planDescStart': MessageLookupByLibrary.simpleMessage('必要なたびに利用してみます。'),
+        'planPriceStart': MessageLookupByLibrary.simpleMessage('25'),
+        'freeLicense': MessageLookupByLibrary.simpleMessage('✓ 無料ライセンス'),
+        'businessLicense': MessageLookupByLibrary.simpleMessage('✓ ビジネスライセンス'),
+        'planConditionStart1':
+            MessageLookupByLibrary.simpleMessage('✓ 低品質デザインを毎月500枚まで作れます'),
+        'planConditionStart2':
+            MessageLookupByLibrary.simpleMessage('✓ 高品質デザインを毎月100枚まで作れます'),
+        'planTitlePremium': MessageLookupByLibrary.simpleMessage('プレミアム'),
+        'planDescPremium': MessageLookupByLibrary.simpleMessage('頻繁に使います。'),
+        'planPricePremium': MessageLookupByLibrary.simpleMessage('95'),
+        'planConditionPremium1':
+            MessageLookupByLibrary.simpleMessage('✓ 低品質デザインを毎月2,000枚まで作れます'),
+        'planConditionPremium2':
+            MessageLookupByLibrary.simpleMessage('✓ 高品質デザインを毎月300枚まで作れます'),
+        'planConditionPremium3':
+            MessageLookupByLibrary.simpleMessage('✓ 最も早い速度で作ります'),
+        'createDesignStartMessage': MessageLookupByLibrary.simpleMessage(
+            'デザイン生成には10～30秒程度かかります。 作成完了後はここまたはギャラリーページで確認可能です。'),
+        'copyShareLink': MessageLookupByLibrary.simpleMessage('共有リンクをコピーしました。'),
+        'style3d': MessageLookupByLibrary.simpleMessage('3Dモデル'),
+        'modernartstyle': MessageLookupByLibrary.simpleMessage('モーダンアート'),
+        'sf': MessageLookupByLibrary.simpleMessage('Sci-Fi'),
+        '3danime': MessageLookupByLibrary.simpleMessage('3Dアニメ'),
+        'mechanic': MessageLookupByLibrary.simpleMessage('メカニック'),
+        'share': MessageLookupByLibrary.simpleMessage('分け合います'),
+        'threeDimensions': MessageLookupByLibrary.simpleMessage('三次元'),
+        'photo': MessageLookupByLibrary.simpleMessage('写真'),
+        'westernFigure': MessageLookupByLibrary.simpleMessage('서양 인물'),
+        'asianFigure': MessageLookupByLibrary.simpleMessage('동양 인물'),
+        'real1': MessageLookupByLibrary.simpleMessage('リアルな写真 1'),
+        'real2': MessageLookupByLibrary.simpleMessage('リアルな写真 2'),
+        'confirm': MessageLookupByLibrary.simpleMessage('確認する'),
+        'bookmark': MessageLookupByLibrary.simpleMessage('お気に入り'),
+        'showImagesFromBookmark':
+            MessageLookupByLibrary.simpleMessage('私がお気に入りした画像を見ることができます。'),
+        'copySharePrompt':
+            MessageLookupByLibrary.simpleMessage('作成命令をコピーしました。'),
+        'reGenerate': MessageLookupByLibrary.simpleMessage('再作成'),
+        'aiEditorExp': MessageLookupByLibrary.simpleMessage(
+            '現在、AI編集機能はPCでのみ提供されていますので、PCで確認をお願いします。今後モバイルでも提供される予定です。'),
+        'cleanerExp': MessageLookupByLibrary.simpleMessage(
+            '不要なオブジェクトを指定して削除することができ、削除された領域は自然に再生成されます。'),
+        'scaleUpExp':
+            MessageLookupByLibrary.simpleMessage('2倍、4倍指定することで解像度を上げることができます。'),
+        'removeBGExp': MessageLookupByLibrary.simpleMessage('背景を透明に削除できます。'),
+        'createAIModel': MessageLookupByLibrary.simpleMessage('AIモデルを作成する'),
+        'createPersonalizationModel':
+            MessageLookupByLibrary.simpleMessage('パーソナライズモデルを作成する'),
+            'youCanCreatePersonalizationModel':
+            MessageLookupByLibrary.simpleMessage('パーソナライズモデルを作成することができます。'),
+            'youCanOnlyInputNumber':
+            MessageLookupByLibrary.simpleMessage('数字のみ入力できます。'),
+        'seedNumberRangeError':
+        MessageLookupByLibrary.simpleMessage('シード番号の範囲を超えました。'),
+        'addSeedNumber':
+        MessageLookupByLibrary.simpleMessage('シード番号を追加する'),
+            'pleaseEnterSeedNumber':
+            MessageLookupByLibrary.simpleMessage('シード番号を入力してください。'),
+        'seedInit':
+        MessageLookupByLibrary.simpleMessage('シード番号を初期化する'),
+        'cancel': MessageLookupByLibrary.simpleMessage('キャンセル'),
+        'noticeDescription': MessageLookupByLibrary.simpleMessage(
+            '11:30から16:00までサーバー移転作業のため、AI作成はできません。'),
+        'notice': MessageLookupByLibrary.simpleMessage('お知らせ'),
+        'neverSeeAgain': MessageLookupByLibrary.simpleMessage('次回から表示しない'),
+
+      };
+}
